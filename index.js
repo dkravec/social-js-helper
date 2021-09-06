1afd3840162e3d747cab089a263146a5c075450a
Daniel Kravec <daniel@kravec.com>
2021-09-06 02:00:37 -0400

    1.0 (1.2021.09.05)

---
+++ index.js
@@ -0,0 +1,130 @@
+const WebSocket = require('ws')
+
+const Client = class {
+    constructor(beta) {
+        console.log(beta)
+        this.beta = beta   
+    }
+
+    login(token, callback) {
+        var ws
+
+        switch (this.beta) {
+            case true:
+                ws = new WebSocket('ws://localhost:5002');
+                break;
+        
+            default:
+                ws = new WebSocket('wss://interact.novapro.net/');
+                break;
+        }
+        ws.onopen = function(event) {
+            ws.send(JSON.stringify({"ready" : "true"}))
+        };
+
+        this.ws = ws
+    }
+    thisTest() {
+        console.log(this)
+    }
+    ready() {
+        this.ws.on('open', function open() {
+            return "Websocket is now running"
+        });
+
+    }
+
+    on(event) {
+        var currentLooking
+        var currentEvent
+        
+        this.ws.on('message', function (dataString) {
+            const data = JSON.parse(dataString)
+            console.log(data)
+            currentEvent = data.type
+        });
+
+        switch (event) {
+            case 'pings': // type 01
+                currentLooking = 1
+                break;
+            case 'message': // type 02
+                currentLooking = 2
+                break;
+            case 'deleteMessages': // type 03
+                currentLooking = 3
+                break;
+            case 'reactions': // type 04
+                currentLooking = 4
+                break
+            case 'editMessages': // type 05
+                currentLooking = 5
+                break
+            case 'userJoin': // type 06
+                currentLooking = 6
+                break;
+            case 'userLeave': // type 07
+                currentLooking = 7
+                break;
+            default:
+                break;
+        }
+        console.log(currentLooking + " " + currentEvent)
+
+    }
+    send(data) {
+        console.log(this.ws.send())
+        this.ws.send(JSON.stringify(data))
+    }
+}
+
+    
+const client = new Client(true)
+client.login(true)
+console.log(client.ready())
+// client.thisTest()
+
+const messageSend = {
+    type: 2,
+    apiVersion: "1.0",
+    message: {
+        content: `This is a test.`	
+    }
+}
+client.send(messageSend)
+
+// client.send(messageSend)
+
+// console.log(client)
+
+// client.on("userLeave")
+// client.on("message")
+
+/*
+const ws = new WebSocket('ws://localhost:5002');
+
+ws.onopen = function(event) {
+    ws.send(JSON.stringify(messageSend))
+};
+*/
+
+// ws.send("hello")
+
+// test()
+function test() {
+    const ws = new WebSocket('ws://localhost:5002');
+
+    ws.onopen = function(event) {
+        console.log("WebSocket is open now.");
+    };
+    const messageSend = {
+        type: 02,
+        apiVersion: "1.0",
+        message: {
+            content: `Hello there, its nice to see you. I am 3rf3, the best bot for interact live chat!`	
+        }
+    }
+
+    ws.send("hello")
+   // ws.send(JSON.stringify({"message" : "test"}))
+}
