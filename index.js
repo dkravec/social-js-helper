2020dc1b48a6c370169241d2fde6227dfe77a89c
Daniel Kravec <daniel@kravec.com>
2022-08-04 23:34:59 -0400

    1.0 (3.2022.08.04)

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 const WebSocket = require('ws')
 require('dotenv').config()
 // helpful info about classes https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+const posibleAPIVersions = ["v1", "v2(fake)"];
+
 const Interact = class {
-    ws = null
-    headerTokens = null
-    environment = null
-    
+    apiURL = 'https://interact-api.novapro.net/'
+
     constructor({beta, environment}) {
         console.log(beta)
         if (
             (environment == 'node') || 
             (environment == 'web')
         ) {
-            this.environment = environment 
+            this.environment = environment;
         } else {
-            this.environment = 'node'
-        }
-    }
+            this.environment = 'node';
+        };
+
+        if (beta) {
+            this.apiURL = "http://localhost:5002/";
+        };
+    };
+
+    setApiVersion({version}) {
+        if (posibleAPIVersions.find(ver=> ver===version)) {
+            this.apiVersion = version;
+            return this.apiVersion;
+        } else {
+            return `The version number provided is not allowed. Please pick from list: ${posibleAPIVersions}`;
+        };
+    };
+    
+    setBeta({beta}) {
+        if (beta===null) {
+            return "Please write if you would like to request. Interact.setBeta() error.";
+        } else if (beta==true) {
+            this.apiURL = "http://localhost:5002/"
+            return `Beta mode is activated. Now using ${this.apiURL} as API url.`;
+        } else if (beta===false) {
+            this.apiURL = "https://interact-api.novapro.net/";
+            return `Beta mode is deactivated. Now using ${this.apiURL} as API url.`;
+        } else {
+            return "Please select an if you would like a beta. true or false. Interact.setBeta() error.";
+        };
+    };
 
     setTokens({ devToken, appToken, accessToken, userToken, userID}) {
         this.headerTokens = {
@@ -25,14 +52,35 @@ const Interact = class {
             accesstoken: accessToken,
             usertoken: userToken,
             userid: userID
-        }
-    }
+        };
+
+        return this.headerTokens;
+    };
 
     checkTokens() {
-        return this.headerTokens
-    }
-}
+        return this.headerTokens;
+    };
 
+    async api({ route, version }) {
+        var useVersion = this.apiVersion;
+        if (version) {
+            if (posibleAPIVersions.find(ver=> ver===version)) {
+                useVersion = version;
+                return this.apiVersion;
+            } else {
+                return `The version number provided is not allowed. Please pick from list: ${posibleAPIVersions}`;
+            };
+        };
+
+        const res = await fetch(`${this.apiURL}${useVersion}/${route}/`, { method: 'GET', headers: this.headerTokens });
+        if (res.error) return { "error" : true, "response ": res }
+        const data = await res.json();
+
+        return { "error" : false, "response" : res, "data" : data };
+    };
+};
+
+/*
 const Client = class {
     constructor(beta) {
         console.log(beta)
@@ -110,23 +158,46 @@ const Client = class {
         this.ws.send(JSON.stringify(data))
     }
 }
+*/
+interacTest();
 
-interacTest()
+async function interacTest() {
+    const interact = new Interact({ beta: true });
+    console.log("-- new interact test");
+    console.log(interact);
 
-function interacTest() {
-    const interact = new Interact(true) 
-    interact.setTokens({
+    const tokenSet = interact.setTokens({
         devToken: process.env.DEVTOKEN,
         appToken: process.env.APPTOKEN,
         accessToken: process.env.ACCESSTOKEN,
         userToken: process.env.USERTOKEN,
         userID: process.env.USERID
-    })
+    });
+    console.log('-- setTokens test');
+    console.log(tokenSet);
 
-    const tokens = interact.checkTokens()
-    console.log(tokens)
-}
+    const tokens = interact.checkTokens();
+    console.log('-- checkTokens test');
+    console.log(tokens);
 
+    const betaEnv = interact.setBeta({ beta: false });
+    console.log('-- setBeta test');
+    console.log(betaEnv);
+
+    const betaEnv2 = interact.setBeta({ beta: true });
+    console.log('-- setBeta test');
+    console.log(betaEnv2);
+
+    const setVersion = interact.setApiVersion({ version: "v1" });
+    console.log('-- setApiVersion test');
+    console.log(setVersion);
+
+    const test = await interact.api({ route: "get/allPosts" });
+    console.log('route test');
+    console.log(test);
+};
+
+/*
 function testmain() {    
     const client = new Client(true)
     client.login(true)
@@ -142,6 +213,7 @@ function testmain() {
     }
     client.send(messageSend)
 }
+*/
 
 // client.send(messageSend)
 
